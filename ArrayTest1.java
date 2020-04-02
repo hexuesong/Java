239962d825d2a00ba7a2b620cc5f2bd03a9f01a2
Bear <877083959@qq.com>
2020-04-02 20:09:32 +0800

    inint files

---
+++ ArrayTest1.java
@@ -0,0 +1,17 @@
+package Day5;
+//±éÀúÊı×é
+public class ArrayTest1 {
+	public static void main(String[] args) {
+		int[] arr = {1,2,3,4,5,6};
+		/*for(int i=0;i<=arr.length-1;i++){
+			System.out.println(arr[i]);
+		}*/
+		getArray(arr);
+	}
+	public static void getArray(int[] x) {
+		// TODO Auto-generated method stub
+		for(int i=0;i<=x.length-1;i++){
+			System.out.println(x[i]);
+		}
+	}
+}
